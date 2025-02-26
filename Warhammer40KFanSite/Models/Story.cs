a19f33a79e2cf1e36fdfcaf2317d86ff27f1b70a
Jason Bruce <46138154+Jasonbruce2001@users.noreply.github.com>
2025-02-26 14:31:34 -0800

    Update

--- Warhammer40KFanSite/Models/Story.cs
+++ Warhammer40KFanSite/Models/Story.cs
@@ -1,11 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Warhammer40KFanSite.Models;
 
 public class Story
 {
     public int StoryID { get; set; }
+    
+    [StringLength(30, MinimumLength = 3)]
+    [Required]
     public string StoryTitle { get; set; }
+    
+    [StringLength(30, MinimumLength = 3)]
+    [Required]
     public string StoryTopic { get; set; }
-    public AppUser StoryAuthor { get; set; }
+    
+    [StringLength(500, MinimumLength = 10)]
+    [Required]
     public string StoryText { get; set; }
+    public AppUser StoryAuthor { get; set; }
     public DateTime StoryDate { get; set; }
 }
