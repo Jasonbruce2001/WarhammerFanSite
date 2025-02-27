b05e05f7995c051a4f1516f0f443017634afc2f1
Jason Bruce <46138154+Jasonbruce2001@users.noreply.github.com>
2025-02-26 18:08:26 -0800

    Added validation and styling

--- Warhammer40KFanSite/Models/Story.cs
+++ Warhammer40KFanSite/Models/Story.cs
@@ -7,15 +7,15 @@ public class Story
     public int StoryID { get; set; }
     
     [StringLength(30, MinimumLength = 3)]
-    [Required]
+    [Required(ErrorMessage = "You must enter a title.")]
     public string StoryTitle { get; set; }
     
     [StringLength(30, MinimumLength = 3)]
-    [Required]
+    [Required(ErrorMessage = "You must enter a topic.")]
     public string StoryTopic { get; set; }
     
-    [StringLength(500, MinimumLength = 10)]
-    [Required]
+    [StringLength(1000, MinimumLength = 10)]
+    [Required(ErrorMessage = "Story text is required.")]
     public string StoryText { get; set; }
     public AppUser StoryAuthor { get; set; }
     public DateTime StoryDate { get; set; }
